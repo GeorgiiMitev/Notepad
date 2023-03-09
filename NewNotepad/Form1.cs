11d7c19ec6aa2e699799aefe4c9fe60e0ec02dc6
NnikolaN <mortinka2017@gmail.com>
2023-03-09 12:46:00 +0200

    Notepad

---
+++ NewNotepad/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewNotepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text File|*.txt|Word Document|*.doc,*.docx|All files|*.*";
+            openFileDialog1.ShowDialog();
+            richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+        }
+
+        private void mnuSaveAs_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text File|*.txt|Word Document|*.doc,*.docx|All files|*.*";
+            saveFileDialog1.FilterIndex = 3;
+            saveFileDialog1.ShowDialog();
+            richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+        }
+
+        private void font_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowColor = true;    
+            fontDialog1.ShowDialog();
+            richTextBox1.SelectionFont = fontDialog1.Font;
+            richTextBox1.SelectionColor = fontDialog1.Color;
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult result = MessageBox.Show(this, "Save this file?", "Save", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.OK)
+                {
+                    mnuSaveAs_Click(sender, e);
+                }
+                if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+                
+
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (richTextBox1.Modified)
+            {
+                DialogResult result = MessageBox.Show("Save this file?", "Save", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    mnuSaveAs_Click(sender, e);
+                }
+                if (result == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+               
+
+            }
+        }
+    }
+}
